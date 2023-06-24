8d57ea39a34129f4579872ab578602d14040de2f
binbin.guan <983529954@qq.com>
2023-06-25 00:14:45 +0800

    regist service

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"miniRpc"
 	"net"
@@ -9,61 +8,51 @@ import (
 	"time"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := miniRpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
 	}
 	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String() //在golang中可以使用channel作为同步的工具。 通过channel可以实现两个goroutine之间的通信。 创建一个channel， make(chan TYPE {, NUM}) , TYPE指的是channel中传输的数据类型，第二个参数是可选的，指的是channel的容量大小。 向channel传入数据， CHAN <- DATA , CHAN 指的是目的channel即收集数据的一方， DATA 则是要传的数据
+	addr <- l.Addr().String()
 	miniRpc.Accept(l)
 }
 
 func main() {
 	log.SetFlags(0)
-	addr := make(chan string) //	创建一个channel， make(chan TYPE {, NUM}) , TYPE指的是channel中传输的数据类型，第二个参数是可选的，指的是channel的容量大小。
+	addr := make(chan string)
 	go startServer(addr)
-
-	//创建连接
 	client, _ := miniRpc.Dial("tcp", <-addr)
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
 	// send request & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		wg.Add(1) // 会加入5个计数
+		wg.Add(1)
 		go func(i int) {
-			defer wg.Done() //执行一个协程，就会减去一个计数
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
-				log.Fatal("call Foo.Sum error:", err) //直接结束了，不会执行下面的代码
+				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
-	wg.Wait() // 会阻塞在这里，直到计数器减为0
-
-	//conn, _ := net.Dial("tcp", <-addr)
-	//defer func() { _ = conn.Close() }()
-	//
-	//time.Sleep(time.Second)
-	//// send options
-	//_ = json.NewEncoder(conn).Encode(miniRpc.DefaultOption)
-	//cc := codec.NewGobCodec(conn)
-	//// send request & receive response
-	//for i := 0; i < 5; i++ {
-	//	h := &codec.Header{
-	//		ServiceMethod: "Foo.Sum",
-	//		Seq:           uint64(i),
-	//	}
-	//	_ = cc.Write(h, fmt.Sprintf("miniRpc req %d", h.Seq))
-	//	_ = cc.ReadHeader(h)
-	//	var reply string
-	//	_ = cc.ReadBody(&reply)
-	//	log.Println("reply:", reply)
-	//}
+	wg.Wait()
 }
